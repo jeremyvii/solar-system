e3290ed83defd294bf7a5243b31937771773812e
Jeremy Jackson <git@jeremyvii.com>
2020-10-12 08:27:45 -0500

    conform to code style

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 /// 1 degree with a calculator.`
 ///
 /// https://www.researchgate.net/publication/45878423_Determining_planetary_positions_in_the_sky_for_50_years_to_an_accuracy_of_1_degree_with_a_calculator
-
 use std::result::Result;
 use std::vec::Vec;
 
@@ -21,79 +20,79 @@ pub use planet::Planet;
 /// to an accuracy of 1 degree. If no date is provided, the current time (utc)
 /// will used.
 struct Cli {
-  /// The epoch at which the planet's position will be determined. If now is
-  /// selected the current date will be used
-  #[structopt(short, long, default_value = "now")]
-  date: String,
+    /// The epoch at which the planet's position will be determined. If now is
+    /// selected the current date will be used
+    #[structopt(short, long, default_value = "now")]
+    date: String,
 
-  /// Which planet to locate. If all is selected, every planet will be printed.
-  #[structopt(short, long, default_value = "all")]
-  planet: String,
+    /// Which planet to locate. If all is selected, every planet will be printed.
+    #[structopt(short, long, default_value = "all")]
+    planet: String,
 }
 
 fn main() {
-  let args = Cli::from_args();
+    let args = Cli::from_args();
 
-  let planet_name = args.planet;
+    let planet_name = args.planet;
 
-  let date = parse_date_str(args.date).expect("Failed to parse provided date");
+    let date = parse_date_str(args.date).expect("Failed to parse provided date");
 
-  if planet_name == "all" {
-    let planets = get_planets();
+    if planet_name == "all" {
+        let planets = get_planets();
 
-    for planet in planets {
-      print_planet_data(planet, date);
-      println!("================");
+        for planet in planets {
+            print_planet_data(planet, date);
+            println!("================");
+        }
+    } else {
+        let planet = get_planet_by_name(planet_name).unwrap();
+
+        print_planet_data(planet, date);
     }
-  } else {
-    let planet = get_planet_by_name(planet_name).unwrap();
-
-    print_planet_data(planet, date);
-  }
 }
 
 /// Retrieve all planets within our solar system
 fn get_planets() -> Vec<Planet> {
-  let mut planets = Vec::new();
+    let mut planets = Vec::new();
 
-  planets.push(Planet::new("Mercury".to_string(), 250.2,     87.969));
-  planets.push(Planet::new("Venus".to_string(),   181.2,     224.701));
-  planets.push(Planet::new("Earth".to_string(),   100.0,     365.256));
-  planets.push(Planet::new("Mars".to_string(),    355.2,     686.98));
-  planets.push(Planet::new("Jupiter".to_string(), 34.3,      4332.59));
-  planets.push(Planet::new("Saturn".to_string(),  50.1,      10759.2));
-  planets.push(Planet::new("Uranus".to_string(),  313.23218, 30687.15));
-  planets.push(Planet::new("Neptune".to_string(), 304.88003, 60190.03));
+    planets.push(Planet::new("Mercury".to_string(), 250.2, 87.969));
+    planets.push(Planet::new("Venus".to_string(), 181.2, 224.701));
+    planets.push(Planet::new("Earth".to_string(), 100.0, 365.256));
+    planets.push(Planet::new("Mars".to_string(), 355.2, 686.98));
+    planets.push(Planet::new("Jupiter".to_string(), 34.3, 4332.59));
+    planets.push(Planet::new("Saturn".to_string(), 50.1, 10759.2));
+    planets.push(Planet::new("Uranus".to_string(), 313.23218, 30687.15));
+    planets.push(Planet::new("Neptune".to_string(), 304.88003, 60190.03));
 
-  return planets;
+    planets
 }
 
 /// Retrieve a planet by it's name
 fn get_planet_by_name(name: String) -> Result<Planet, &'static str> {
-  for planet in get_planets() {
-    if planet.name == name {
-      return Ok(planet);
+    for planet in get_planets() {
+        if planet.name == name {
+            return Ok(planet);
+        }
     }
-  }
 
-  return Err("No planet found by that name.");
+    Err("No planet found by that name.")
 }
 
 /// Attempts to parse the provided string into a `DateTime<Utc>` instance
 fn parse_date_str(date_str: String) -> Result<DateTime<Utc>, &'static str> {
-  if date_str == "now" {
-    return Ok(Utc::now());
-  }
+    if date_str == "now" {
+        return Ok(Utc::now());
+    }
 
-  let naive_date = NaiveDate::parse_from_str(&date_str.to_string(), "%Y-%m-%d");
+    let naive_date = NaiveDate::parse_from_str(&date_str, "%Y-%m-%d");
 
-  if naive_date.is_err() {
-    return Err("Failed to parse provided date.");
-  }
+    if naive_date.is_err() {
+        return Err("Failed to parse provided date.");
+    }
 
-  let naive_datetime = naive_date.unwrap().and_hms(0, 0, 0);
+    let naive_datetime = naive_date.unwrap().and_hms(0, 0, 0);
 
-  return Ok(DateTime::from_utc(naive_datetime, Utc));
+    Ok(DateTime::from_utc(naive_datetime, Utc))
 }
 
 /// Prints a planet's data
@@ -101,10 +100,10 @@ fn parse_date_str(date_str: String) -> Result<DateTime<Utc>, &'static str> {
 /// Prints a planet's name, astronomical units, orbital period in years, and
 /// position in degrees.
 fn print_planet_data(planet: Planet, date: DateTime<Utc>) {
-  let position = planet.get_position_at_date(date);
+    let position = planet.get_position_at_date(date);
 
-  println!("Planet:\t\t\t\t{0}", planet.name);
-  println!("Astronomical Units:\t\t{0}", planet.get_au());
-  println!("Years to orbit the sun:\t\t{0}", planet.get_years());
-  println!("Planet {0} is located at:\t{1}", planet.name, position);
+    println!("Planet:\t\t\t\t{0}", planet.name);
+    println!("Astronomical Units:\t\t{0}", planet.get_au());
+    println!("Years to orbit the sun:\t\t{0}", planet.get_years());
+    println!("Planet {0} is located at:\t{1}", planet.name, position);
 }
--- src/planet.rs
+++ src/planet.rs
@@ -1,14 +1,11 @@
-pub use self::planet::Planet;
+extern crate chrono;
+use chrono::prelude::{DateTime, TimeZone, Utc};
 
-mod planet {
-  extern crate chrono;
-  use chrono::prelude::{DateTime, Utc, TimeZone};
+/// The number of days lapsed before the Earth completes one orbit
+const YRS: f64 = 365.256;
 
-  /// The number of days lapsed before the Earth completes one orbit
-  const YRS: f64 = 365.256;
-
-  /// Represents a planet
-  pub struct Planet {
+/// Represents a planet
+pub struct Planet {
     /// The planet's name
     pub name: String,
 
@@ -19,47 +16,46 @@ mod planet {
     pub period: f64,
 
     /// The reference to compare the provided date to
-    pub ref_date: DateTime<Utc>
-  }
+    pub ref_date: DateTime<Utc>,
+}
 
-  impl Planet {
+impl Planet {
     /// Constructs a new planet
     pub fn new(name: String, mean_longitude: f64, period: f64) -> Planet {
-      Planet {
-        name: name,
-        mean_longitude: mean_longitude,
-        period: period,
-        ref_date: Utc.ymd(2000, 1, 1).and_hms(0, 0, 0),
-      }
+        Planet {
+            name,
+            mean_longitude,
+            period,
+            ref_date: Utc.ymd(2000, 1, 1).and_hms(0, 0, 0),
+        }
     }
 
     /// Returns the planet's angular speed
     pub fn get_angular_speed(&self) -> f64 {
-      return 360.0 / self.period;
+        360.0 / self.period
     }
 
     /// Returns the planet's distance from the sun in Astronomical Units
     pub fn get_au(&self) -> f64 {
-      return self.get_years().powf(2.0).cbrt();
+        self.get_years().powf(2.0).cbrt()
     }
 
     /// Retrieves how many Earth years a planet will take to orbit the sun
     pub fn get_years(&self) -> f64 {
-      return self.period / YRS;
+        self.period / YRS
     }
 
     /// Determines the planets position in degrees at a given date
     pub fn get_position_at_date(&self, date: DateTime<Utc>) -> f64 {
-      let timestamp = date.timestamp();
+        let timestamp = date.timestamp();
 
-      let ref_timestamp = self.ref_date.timestamp();
+        let ref_timestamp = self.ref_date.timestamp();
 
-      let date_diff = (ref_timestamp - timestamp) / (24 * 60 * 60 * 1000);
-      let date_diff_float = date_diff as f64;
+        let date_diff = (ref_timestamp - timestamp) / (24 * 60 * 60 * 1000);
+        let date_diff_float = date_diff as f64;
 
-      let angle_traversed = self.get_angular_speed() * date_diff_float;
+        let angle_traversed = self.get_angular_speed() * date_diff_float;
 
-      return (self.mean_longitude * angle_traversed) % 360.0;
+        (self.mean_longitude * angle_traversed) % 360.0
     }
-  }
 }
